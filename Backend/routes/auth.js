9444bd1e84384f21a06c5e3bc13e3a016743164b
akshat-2502 <khulbeakshat@gmail.com>
2025-06-19 17:19:31 +0530

    authentication

---
+++ Backend/routes/auth.js
@@ -0,0 +1,9 @@
+const User = require("../models/user");
+const router = require("express").Router();
+
+router.post("/register", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+module.exports = router;
